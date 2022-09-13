fb52f4f69f1da5bb6b861f58ac7cd2848cf701c7
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-13 21:53:26 +0300

    testing api

--- send.php
+++ send.php
@@ -1,3 +1,14 @@
 <?php
 
+include("connection.php");
+
+$sql = "INSERT INTO message (name, email, phone, message)
+VALUES ('John', 'john@example.com',71371723,'heklsldsadasjknaskd    ')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 ?>
