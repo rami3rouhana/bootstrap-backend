79150b4a2db1ba9cc8c2ee3e8e00545d3036d11f
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-13 21:57:57 +0300

    fix api call

--- send.php
+++ send.php
@@ -2,8 +2,7 @@
 
 include("connection.php");
 
-$sql = "INSERT INTO message (name, email, phone, message)
-VALUES ('John', 'john@example.com',71371723,'heklsldsadasjknaskd    ')";
+$sql = "INSERT INTO messages (name, email, phone, message) VALUES ('John', 'john@example.com',71371723,'hekls ldsa dasjkn askd')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
